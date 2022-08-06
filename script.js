85f5c881e3b5290495edcc94c9a5e48da4b9f871
OscarOrtiz <tippingtheiceberg@gmail.com>
2022-08-06 11:53:22 -0500

    add animation for confirmation click, begin implementation of username throughout site

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
+// Confirm User Opacity change 
+let mainUserNameContainer = document.querySelector(".userNameMain__container");
 let userNameInput = document.querySelector("#userName__input")
 let userNameText = document.querySelector("#userName__text")
-let confirmUser = document.querySelector(".confirmUser__container")
+let userNameContainer = document.querySelector(".confirmUser__container")
 userNameInput.addEventListener("input",()=>{
     userNameText.textContent = userNameInput.value;
-    userNameInput.value != ""? confirmUser.style.opacity = "1": confirmUser.style.opacity = "0"
-    
+    userNameInput.value != ""? userNameContainer.style.opacity = "1": userNameContainer.style.opacity = "0" 
 })
-console.log(userNameInput.value);
-console.log(userNameText)
+// Confirmation of user name
+let confirmUserName = document.querySelector(".confirmUser__yes")
+let cancelUserName = document.querySelector(".confirmUser__no")
+
+confirmUserName.addEventListener("click", () =>{
+mainUserNameContainer.style.cssText = "transform:translate3d(-500px,0px,0px); opacity:0";
+})
